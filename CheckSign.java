e8d3fa1b8a1f47a2fea6f6758433703b4e259522
farithabegam <47548743+farithabegam@users.noreply.github.com>
2019-02-21 14:25:22 +0530

    Create CheckSign.java

---
+++ CheckSign.java
@@ -0,0 +1,14 @@
+class checksign{
+public static void main(string[] qwe)}
+int a=10;
+if(a<0){
+sysytem.out.println("negative");
+}
+else if(a==0){
+sysytem.out.println("zero");
+}
+else{
+system.out.println("positive");
+}
+}
+}
